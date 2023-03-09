2085530456b2314c436ae4dfb910216b49ac4046
nazlican-245 <116080055+nazlican-245@users.noreply.github.com>
2023-03-09 19:13:49 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const { default: axios } = require("axios");
+
+async function getVeri(n) {
+
+    const users = async (n) => {
+        const { veri } = await axios("https://jsonplaceholder.typicode.com/users/" + n);
+        console.log(veri);
+}
+
+
+    const post = async (n) => {
+        const { veri1 } = await axios("https://jsonplaceholder.typicode.com/posts?id=" + n);
+        console.log(veri1);
+    }
+const userOfId = await users(n)
+const userOfPost = await post(n)
+return userOfId + userOfPost
+}
+
+
+export default getVeri;
